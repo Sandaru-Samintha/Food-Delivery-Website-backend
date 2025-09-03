cb72e51a38360f5939aa0eff87007f8954604f44
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2025-09-03 21:15:00 +0530

    create the server

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express"
+import cors from "cors"
+
+//app config
+const app = express()  //initialice the app using the express
+const port = 4000      //define the port number where our server will be running
+
+//middleware
+app.use(express.json())  //when never we will get the request from frontend the backend that will be pass using the express.json file
+app.use(cors()) //using this we can access the backend the any frontend
+
+
+app.get("/",(req,res)=>{
+  res.send("API Working")
+}) //using this get method using http request data from server ,update ,delete method are depend for this
+
+app.listen(port,()=>{
+  console.log(`Server Started on http://localhost:${port}`)
+})//using this run the express server from using port number
