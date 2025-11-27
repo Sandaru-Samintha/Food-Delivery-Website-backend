4e3e8087592c46065c4a3cba3fb745d3700cb5bf
Sandaru Samintha <sandarusaminthagunarathna@gmail.com>
2025-11-27 12:25:47 +0530

    create the getCart function in the cartController page

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const app = express()  //initialice the app using the express
 const port = 4000      //define the port number where our server will be running
 
 //middleware
-app.use(express.json())  //when never we will get the request from frontend the backend that will be pass using the express.json file
-app.use(cors()) //using this we can access the backend the any frontend
+app.use(express.json()); //when never we will get the request from frontend the backend that will be pass using the express.json file
+app.use(cors());//using this we can access the backend the any frontend
 
 
 //db connection
